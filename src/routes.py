f1a43f710bfb53427ea0d4b585b6b53156ba65c7
Alonso Suarez <alonchb@gmail.com>
2014-01-24 15:19:58 -0700

    divided into separated files

--- src/routes.py
+++ src/routes.py
@@ -1,16 +1,45 @@
 import os, urllib, webapp2, jinja2, db
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/view/html"),
     extensions=['jinja2.ext.autoescape'],
-    autoescape=True)
+    autoescape=False)
+
+def get_header(page):
+    content = {}
+    user = {}
+    user['is_logged'] = False
+    user['nickname'] = 'Alonch'
+    content['user'] = user
+    content['page'] = page
+    return env.get_template('default/header.html').render(content)
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-    	template = JINJA_ENVIRONMENT.get_template('view/index.html')
-        self.response.write(template.render())
+    	content = {}
+    	index = env.get_template('index.html')
+    	content['head'] = env.get_template('default/head.html').render()
+    	content['header'] = get_header('/')
+
+    	keys = ['home','events','whatdowedo','info']
+    	for key in keys:
+    		content[key] = env.get_template('index/%s.html' % key).render()
+        self.response.write(index.render(content))
         
+class SignUpPage(webapp2.RequestHandler):
+
+    def get(self):
+    	content = {}
+    	content['head'] = env.get_template('default/head.html').render()
+    	content['header'] = get_header("sign-up")
+    	index = env.get_template('sign-up.html')
+
+    	self.response.write(index.render(content))
+        
+
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/sign-up', SignUpPage)
 ], debug=True)
