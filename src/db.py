7ed438a428336c157daa1026a94883a492562a2b
Alonso Suarez <alonchb@gmail.com>
2014-02-03 12:11:26 -0700

    Finished mentor sign up

--- src/db.py
+++ src/db.py
@@ -1,19 +1,18 @@
 from google.appengine.ext import ndb
-
-class PersonType():
-	kid = "Kid"
-	mentor = "Mentor"
+from datetime import datetime
+import json
+import logging
 
 class Person(ndb.Model):
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
-    birthdate = ndb.DateProperty()
     phone = ndb.StringProperty()
     email = ndb.StringProperty()
     liability_signed = ndb.BooleanProperty()
     media_signed = ndb.BooleanProperty()
     tshirt_size = ndb.StringProperty()
     personType = ndb.StringProperty()
+    gender = ndb.StringProperty()
 
 class Kid(ndb.Model):
 	person = ndb.KeyProperty()
@@ -21,21 +20,22 @@ class Kid(ndb.Model):
 	grade = ndb.StringProperty()
 	school = ndb.StringProperty()
 	recomended = ndb.BooleanProperty()
+	birthdate = ndb.DateProperty()
 
 class Mentor(ndb.Model):
-	person = ndb.KeyProperty()
 	#which university or company
 	affiliation = ndb.StringProperty()
 	#First Year, Graduated, Etc.
 	affiliation_status = ndb.StringProperty()	
 	#Electrical, Computer Science
 	discipline = ndb.StringProperty()
-	is_actave = ndb.BooleanProperty()
+	_active = ndb.BooleanProperty()
 	police_done = ndb.BooleanProperty()
 	police_comments = ndb.StringProperty()
 	police_date = ndb.DateProperty()
 	references_ok = ndb.BooleanProperty()
 	references_comments = ndb.StringProperty()
+	cpr_trained = ndb.BooleanProperty()
 
 class Event(ndb.Model):
 	start = ndb.DateTimeProperty()
@@ -50,5 +50,34 @@ class Attendance(ndb.Model):
 
 
 
+def gql_json_parser(query_obj):
+    result = []
+    for entry in query_obj:
+        result.append(dict([(p, unicode(getattr(entry, p))) for p in get_keys(entry.__class__)]))
+    return result
 
+def get_keys(cls):
+	attr = []
+	for key in dir(cls):
+            if "key" == key or key[0] == '_':
+                continue
+            if isinstance(getattr(cls, key),ndb.Property):
+                attr.append(key)
+	return attr
 
+def request_python_parser(cls, get):
+	attr = get_keys(cls.__class__)
+	logging.info("attr:%s" % str(attr))
+	for key in attr:
+		value = get(key)
+
+		typ = type(getattr(cls.__class__, key))
+		logging.info("key:%s, type:%s" % (key, str(typ)))
+		if typ == ndb.BooleanProperty:
+			value = value != ''
+		elif typ == ndb.DateProperty:
+			value = datetime.strptime(value, '%Y-%m-%d').date() #2014-02-02
+		#elif typ == ndb.:
+		#	return int(value)
+
+		setattr(cls, key, value)
--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
-import os, urllib, webapp2, jinja2, db
+import os, urllib, webapp2, jinja2, db, json
+from google.appengine.ext import ndb
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/view/html"),
@@ -27,7 +28,7 @@ class MainPage(webapp2.RequestHandler):
     	for key in keys:
     		content[key] = env.get_template('index/%s.html' % key).render()
         self.response.write(index.render(content))
-        
+
 class SignUpPage(webapp2.RequestHandler):
 
     def get(self):
@@ -36,12 +37,56 @@ class SignUpPage(webapp2.RequestHandler):
     	content['header'] = get_header("sign-up")
     	content['js'] = env.get_template('default/js.html').render()
     	index = env.get_template('sign-up.html')
-
+        keys = ['mentee', 'mentor']
+        for key in keys:
+            content[key] = env.get_template('sign-up/%s.html' % key).render()
     	self.response.write(index.render(content))
         
+    def post(self):
+        get = self.request.get
+        person = db.Person()
+        db.request_python_parser(person, get)
 
+        mentor = db.Mentor()
+
+        #for key in mentor.keys:
+         #   pass
+            #setattr(mentor, key, get(key))
+        person.put()
+        #mentor.put()
+        self.response.write("ok")
+
+  
+class ShowMentorPage(webapp2.RequestHandler):
+
+    def get(self):
+        query_data = db.Person.query()
+        json_query_data = db.gql_json_parser(query_data)
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(json_query_data))
+
+class ClearMentorPage(webapp2.RequestHandler):
+
+    def get(self):
+        query_data = db.Person.query()
+        for entry in query_data:
+            entry.key.delete()
+        self.response.write("ok")
+
+class TestPage(webapp2.RequestHandler):
+
+    def get(self):
+        cls = db.Person().__class__
+        for key in dir(cls):
+            if key[0] == "_":
+                continue
+            if isinstance(getattr(cls, key),ndb.Property):
+                self.response.write(key +'<br>' )
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign-up', SignUpPage)
+    ('/sign-up', SignUpPage),
+    ('/mentors', ShowMentorPage),
+    ('/mentors/clear', ClearMentorPage),
+    ('/test', TestPage)
 ], debug=True)
